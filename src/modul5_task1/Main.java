ca64359a4736692821ed1371206f53bfb6ff8b9c
Артем <artem.tsaplya18@gmail.com>
2015-12-29 12:02:33 +0200

    modul5_task1

---
+++ src/modul5_task1/Main.java
@@ -0,0 +1,15 @@
+package modul5_task1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int massive [] = new int[] {2, -4, 19, 26, -7};
+        Arrays.sort(massive);
+        System.out.println("Min " + massive[0]);
+        System.out.println("Max " + massive[4]);
+
+        }
+
+    }
+
