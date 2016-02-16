842481634e9fdbb4020dc80c65fc0c997d5ca906
Артем <artem.tsaplya18@gmail.com>
2016-02-16 20:00:50 +0200

    modul9

--- src/modul9_caesar/CaesarCode.java
+++ src/modul9_caesar/CaesarCode.java
@@ -1,32 +1,38 @@
 package modul9_caesar;
 
+import java.util.Arrays;
+
 public class CaesarCode {
-    public  void caesarEncode(String stringEncode, int mod) {
-        char[] stringArray = stringEncode.toCharArray();
-        for (int i = 0; i < stringArray.length; i++) {
-            char a = stringArray[i];
-            int b = (int) a;                              // переводим текст в символы по ASCII
-            b = b + mod;                                 // измененяем значение по методу шифрования Цезаря
-            char r = (char) b;                   // и сохраняем в массив
-            stringArray[i] = r;
+
+    public String caesarEncode(String stringEncode, int mod) {
+        char[] charArray = stringEncode.toCharArray();
+        int i = 0;
+        for (char c : charArray) {
+            int charToASCII = (int) c;                              // переводим текст в символы по ASCII
+            charToASCII = charToASCII + mod;                                 // измененяем значение по методу шифрования Цезаря
+            char ASCIItoChar = (char) charToASCII;                   // и сохраняем в массив
+            charArray[i] = ASCIItoChar;
+            i++;
         }
-        System.out.print("Encryption text: ");
-        for (char c : stringArray) {
-            System.out.print(c);
+        StringBuilder builder = new StringBuilder();
+        for (char c : charArray){
+            builder.append(c);
         }
+        return builder.toString();
     }
-    public void caesarDecode(String stringDecode, int mod){
+
+    public void caesarDecode(String stringDecode, int mod) {
         char[] stringArray = stringDecode.toCharArray();
         for (int y = 0; y < stringArray.length; y++) {
-            char a =  stringArray[y];
-            int b = (int)a;                              // переводим текст в символы по ASCII
+            char a = stringArray[y];
+            int b = (int) a;                              // переводим текст в символы по ASCII
             b = b - mod;                                 // измененяем значение по методу шифрования Цезаря
             char r = (char) b;                   // и сохраняем в массив
             stringArray[y] = r;
         }
         System.out.println();
         System.out.print("Decoding text :");
-        for (char v: stringArray){
+        for (char v : stringArray) {
             System.out.print(v);
         }
     }
--- src/modul9_caesar/Main.java
+++ src/modul9_caesar/Main.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Please enter text for Encryption: ");
-        Scanner scanner = new Scanner(System.in);
-        String encodeText = scanner.nextLine();
-        System.out.print("Enter mode: ");
-        int encodeMode = scanner.nextInt();
-
-        System.out.print("Please enter text for Decoding: ");
-        Scanner scanner2 = new Scanner(System.in);
-        String decodeText = scanner2.nextLine();
-        System.out.print("Enter mode: ");
-        int decodeMod = scanner2.nextInt();
+//        System.out.print("Please enter text for Encryption: ");
+//        Scanner scanner = new Scanner(System.in);
+//        String encodeText = scanner.nextLine();
+//        System.out.print("Enter mode: ");
+//        int encodeMode = scanner.nextInt();
+//
+//        System.out.print("Please enter text for Decoding: ");
+//        Scanner scanner2 = new Scanner(System.in);
+//        String decodeText = scanner2.nextLine();
+//        System.out.print("Enter mode: ");
+//        int decodeMod = scanner2.nextInt();
 
         CaesarCode caesarCode = new CaesarCode();
-        caesarCode.caesarEncode(encodeText,encodeMode);
-        caesarCode.caesarDecode(decodeText,decodeMod);
+        int encodeMode = 3;
+        String encodeText = "abc";
+        String text = caesarCode.caesarEncode(encodeText,encodeMode);
+        System.out.println(text);
     }
 }
