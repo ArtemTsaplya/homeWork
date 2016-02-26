68096715ba89d4fef9184a28156c4134566fabbf
ArtemTsaplya <artem.tsaplya18@gmail.com>
2016-02-26 15:20:38 +0200

    modul

--- src/main/java/practic_1/Main.java
+++ src/main/java/practic_1/Main.java
@@ -2,9 +2,9 @@ package practic_1;
 
 public class Main {
     public static void main(String[] args) {
-        SumNumber number = new SumNumber();
-        int array[] = {10,45,23};
-        int sum = number.max(array);
-        System.out.println(sum);
+        SnakeToMatrix toSnake = new SnakeToMatrix();
+        int[][] array = {{10, 45, 23}, {1, 4, 3}, {0, 5, 8}};
+        int result[] = toSnake.snake(array);
+        System.out.println(result);
     }
 }
